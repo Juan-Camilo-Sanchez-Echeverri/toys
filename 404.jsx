1600ee8914a676c4204d89fbc7ec64fc56a510ac
Juan Camilo Sanchez Echeverri <juancamilosanche65@gmail.com>
2023-06-27 15:45:11 -0500

    Updates

--- 404.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import PaginaNoEncontrada from '../src/pages/PaginaNoEncontrada'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PaginaNoEncontrada />
-  </React.StrictMode>,
-)
