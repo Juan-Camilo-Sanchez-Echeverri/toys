edac437f01ce9f552167caf1caa477082e9d917a
Juan Camilo Sanchez Echeverri <juancamilosanche65@gmail.com>
2023-06-27 15:41:07 -0500

    Updates

---
+++ 404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PaginaNoEncontrada from '../src/pages/PaginaNoEncontrada'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PaginaNoEncontrada />
+  </React.StrictMode>,
+)
